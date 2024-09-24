16cec469acdd55f48bd5cd373babf485fcdd5565
rondDev <contact@rond.cc>
2024-09-24 19:42:18 +0200

    fix: change LC to a string

--- src/log.rs
+++ src/log.rs
@@ -1,11 +1,9 @@
 pub use crate::{debug, error, info, trace, warn};
 
-use std::sync::Arc;
-
 /// Log Context
 #[derive(Clone)]
 pub struct LC {
-    pub name: Arc<str>, // TODO: make this not a arc
+    pub name: String, // TODO: make this not a arc
     pub should_log: bool,
 }
 
@@ -34,7 +32,7 @@ impl LC {
     }
 }
 
-use ::std::fmt::{Display, Error as FmtError, Formatter};
+use std::fmt::{Display, Error as FmtError, Formatter};
 impl Display for LC {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), FmtError> {
         write!(f, "{}", self.name)
