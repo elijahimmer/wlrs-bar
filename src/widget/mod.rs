04e768d22a52c2f0303679e379604087cd758bff
Elijah M. Immer <me@eimmer.me>
2024-09-26 20:39:45 -0700

    widget: Add more documentation to the widget interfaces

--- src/widget/mod.rs
+++ src/widget/mod.rs
@@ -8,36 +8,67 @@ use crate::log::*;
 use anyhow::Result;
 
 pub trait Widget {
+    /// Returns what context you should log for the widget
     fn lc(&self) -> &LC;
+
+    /// Returns the Area that widget uses
     fn area(&self) -> Rect;
+
+    /// Returns the horizontal alignment
     fn h_align(&self) -> Align;
+
+    /// Returns the vertical alignment
     fn v_align(&self) -> Align;
+
+    /// Returns the desired height of the widget
     fn desired_height(&self) -> u32;
+
+    /// Returns the desired width of the widget
     fn desired_width(&self, height: u32) -> u32;
 
+    /// Force the widget to use the new area given.
     fn resize(&mut self, rect: Rect);
+
+    /// Whether or not the widget should be redrawn
     fn should_redraw(&mut self) -> bool;
+
+    /// Draw the widget in the DrawCtx
     fn draw(&mut self, ctx: &mut DrawCtx) -> Result<()>;
 
+    /// Clicks the widget at a specific (global) point
     fn click(&mut self, button: ClickType, point: Point) -> Result<()>;
+
+    /// Says that the cursor was moved into or within the widget
     fn motion(&mut self, point: Point) -> Result<()>;
+
+    /// Says the cursor left the widget
     fn motion_leave(&mut self, point: Point) -> Result<()>;
 }
 
 pub trait PositionedWidget {
+    /// Return the top internal margin of the widget
     fn top_margin(&self) -> u32;
+
+    /// Return the bottom internal margin of the widget
     fn bottom_margin(&self) -> u32;
+
+    /// Return the left internal margin of the widget
     fn left_margin(&self) -> u32;
+
+    /// Return the right internal margin of the widget
     fn right_margin(&self) -> u32;
 
+    /// Return the combined top and bottom internal margin of the widget
     fn v_margins(&self) -> u32 {
         self.top_margin() + self.bottom_margin()
     }
 
+    /// Return the combined left and right internal margin of the widget
     fn h_margins(&self) -> u32 {
         self.left_margin() + self.right_margin()
     }
 
+    /// Returns the x, y pair of the internal margins.
     fn margins(&self) -> Point {
         Point {
             x: self.h_margins(),
@@ -66,12 +97,7 @@ impl ClickType {
     }
 }
 
-//pub trait Builder {
-//    type Widget;
-//    fn new() -> Self;
-//    fn build(&self, name: &str) -> Self::Widget;
-//}
-
+/// Automatically makes the boilerplate constructor setters
 #[macro_export]
 macro_rules! builder_fields {
     ($($t: ty, $($n: ident)+;)+) => ($($(
